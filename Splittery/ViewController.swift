3855fbc99fd49d30bfdfb93096aa85fa245de76e
Maxim Bystrov <maxibystro@gmail.com>
2019-02-02 13:19:29 +0300

    Add project

---
+++ Splittery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Splittery
+//
+//  Created by Maxim Bystrov on 02/02/2019.
+//  Copyright © 2019 Maxim Bystrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
